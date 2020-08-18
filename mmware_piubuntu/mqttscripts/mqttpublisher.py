0547b4cc25b38bb807a72a898a7502384a009608
sbsoumya <soumya.s.banerjee17@gmail.com>
2020-08-18 21:58:07 +0530

    Addition of piubuntu

---
+++ mmware_piubuntu/mqttscripts/mqttpublisher.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt #import the client1
+broker_address="piubuntu" 
+topic = "mqttsample"
+#broker_address="iot.eclipse.org"
+print("creating new instance")
+client = mqtt.Client("P1") #create new instance
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+#print("Subscribing to topic","house/bulbs/bulb1")
+#client.subscribe("house/bulbs/bulb1")
+print("Publishing message to topic",topic)
+while True:
+	message=input("Enter message:")
+	print (message)
+	client.publish(topic,message)
